b65682c2873ebaf1b3a507af89dea238dfd9e349
hunglocker <hungdzai404@gmail.com>
2026-02-04 10:29:32 +0700

    Update code

--- src/main.java
+++ src/main.java
@@ -4,48 +4,35 @@ import java.util.Scanner;
 
 
 public class main {
+    private static List<Integer> list = new ArrayList<>();
+    private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-         System.out.println("Vui lòng chọn chức năng dưới đây");
-         System.out.println("Chức năng 1 : Nhập dữ liệu vào từ bàn phím và lưu trữ dữ liệu vào danh sách (List)");
-         System.out.println("Chức năng 2:  Tính tổng giá trị trong danh sách");
-         System.out.println("Chức năng 3: Tìm ra giá trị lớn nhất trong danh sách");
-         System.out.println("Chức năng 4: Hiển thị các giá trị trong danh sách");
-         System.out.println("Chức năng 5: Lưu trữ thông tin vào trong file json, xml");
-         Scanner sc = new Scanner(System.in);
-         int choice = sc.nextInt();
+         int choice = 0;
+        do {
+            System.out.println("Vui lòng chọn chức năng dưới đây");
+            System.out.println("Chức năng 1 : Nhập dữ liệu vào từ bàn phím và lưu trữ dữ liệu vào danh sách (List)");
+            System.out.println("Chức năng 2:  Tính tổng giá trị trong danh sách");
+            System.out.println("Chức năng 3: Tìm ra giá trị lớn nhất trong danh sách");
+            System.out.println("Chức năng 4: Hiển thị các giá trị trong danh sách");
+            System.out.println("Chức năng 5: Lưu trữ thông tin vào trong file json, xml");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1: InputData();
+                    break;
 
-         switch (choice) {
-             case 1: InputData();
-             break;
-             case 2:
-                 System.out.print("Chưa làm");
-                 break;
-
-         }
+            }
+        } while(choice != 0);
 
 
     }
 
     public static void InputData() {
-        List<Integer> list = new ArrayList<>();
-        while(true) {
-            Scanner input = new Scanner(System.in);
-            int num = input.nextInt();
-            list.add(num);
-            System.out.println(list);
-        }
-    }
+        System.out.println("Nhập dữ liệu vào mảng");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        list.add(num);
 
-//    public static void inputner() {
-//         while(true) {
-//             Scanner sc = new Scanner(System.in);
-//             int number = sc.nextInt();
-//             list.add(number);
-//         }
-//
-//
-//
-//    }
+    }
 
 
 }
